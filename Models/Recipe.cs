a2d5a40c77122a7d3c2f48464a44ddbc30f44206
ae.dashkov <water.universe@yandex.ru>
2025-05-26 18:08:15 +0300

    new class Recipe

---
+++ Models/Recipe.cs
@@ -0,0 +1,9 @@
+﻿namespace CookieBookieBot.Models
+{
+    internal class Recipe
+    {
+        public string Name { get; private set; } 
+        public string Dascription { get; private set; }
+        public List<string> Ingredients { get; private set; }
+    }
+}
