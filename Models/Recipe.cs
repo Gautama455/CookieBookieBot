0317644dbd7da7d1cc9831c0d07934a73d06c6a7
ae.dashkov <water.universe@yandex.ru>
2025-05-27 17:59:18 +0300

    new fields in Recipe

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -1,9 +1,35 @@
-﻿namespace CookieBookieBot.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CookieBookieBot.Models
 {
     internal class Recipe
     {
-        public string Name { get; private set; } 
-        public string Dascription { get; private set; }
-        public List<string> Ingredients { get; private set; }
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public List<Ingredient> Ingredients { get; private set; }
+        public string Category { get; private set; }
+        public string Difficulty { get; private set; }
+        public string Author { get; private set; }
+        public List<string> Steps { get; private set; }
+        public string Image { get; private set; }
+        public string DateOfCreated { get; private set; }
+
+        [JsonConstructor]
+        public Recipe(string id, string name, string description, List<Ingredient> ingredients,
+            string category, string difficulty, string author, List<string> steps,
+            string image, string dateOfCreated)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Ingredients = ingredients;
+            Category = category;
+            Difficulty = difficulty;
+            Author = author;
+            Steps = steps;
+            Image = image;
+            DateOfCreated = dateOfCreated;
+        }
     }
 }
