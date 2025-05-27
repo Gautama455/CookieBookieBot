0d7bc76dd6b9abd0de0912720e44742bc68f5a97
ae.dashkov <water.universe@yandex.ru>
2025-05-27 17:58:46 +0300

    new class Ingredient

---
+++ Models/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CookieBookieBot.Models
+{
+    internal class Ingredient
+    {
+        public string Name { get; private set; }    
+        public int Quantity { get; private set; }
+        public string Unit { get; private set; }
+
+        [JsonConstructor]
+        public Ingredient(string name, int quantity, string item)
+        {
+            Name = name;
+            Quantity = quantity;
+            Unit = item;
+        }
+    }
+}
