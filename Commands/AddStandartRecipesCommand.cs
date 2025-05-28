a56321b69a1a192b93d45d3f016d6efea0262c8d
ae.dashkov <water.universe@yandex.ru>
2025-05-28 16:44:53 +0300

    new command AddStandartRecipeCommand

---
+++ Commands/AddStandartRecipesCommand.cs
@@ -0,0 +1,31 @@
+﻿using CookieBookieBot.Data;
+using Telegram.Bot;
+
+namespace CookieBookieBot.Commands
+{
+    internal class AddStandartRecipesCommand : BotCommand
+    {
+        public override string Command => "/add_standrt_recipes";
+
+        public override async Task Run(ITelegramBotClient botClient, Telegram.Bot.Types.Message message, CancellationToken ct)
+        {
+            try
+            {
+                JsonBuilder.BuildRecipe(message);
+                await botClient.SendMessage(
+                    chatId: message.Chat.Id,
+                    text: "Стандартные рецепты успешно добавлены",
+                    cancellationToken: ct
+                    );
+            }
+            catch (Exception ex)
+            {
+                await botClient.SendMessage(
+                    chatId: message.Chat.Id,
+                    text: $"Ошибка при добавлени рецепта: {ex.Message}",
+                    cancellationToken: ct
+                    );
+            }
+        }
+    }
+}
