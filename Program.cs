92b686d28763653226eb1e3b0503e6be597e1565
ae.dashkov <water.universe@yandex.ru>
2025-05-28 16:50:39 +0300

    1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        string token = "7618715792:AAFS_-1mlpDiDpLyfmmQQ6K0lbhEl-7XJNo";
+        string token = "token";
 
         var botApp = new BotApplication(token);
 
